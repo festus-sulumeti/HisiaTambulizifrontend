4eac4a8a9011ef6cd047752722912539806d902a
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-03 22:22:58 +0300

    the login css

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 // import config from "../config/config";
 
 
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
