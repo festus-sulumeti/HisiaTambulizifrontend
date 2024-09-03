0bf033e8e8492fd8058f2c7ed2791b38926abf54
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-03 20:31:48 +0300

    the project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import { ArrowRight } from 'lucide-react';
+import "../styling/Home.css"
+
+
 const Home = () => {
     return (
-        <div>
-          <h1>Emotion Recognition speech</h1>
+        <div className="home-container">
+          <h1 className="home-heading">Emotion Recognition speech</h1>
+          <p>Emotion Recognition speech is a web application that uses the Web Speech API to recognize speech and then uses the Microsoft Azure Emotion API to analyze the emotions in the recognized speech.</p>
+
+
+          <div>
+            <Link to={"/login"}>Login <ArrowRight className= "arrow-icon" /> </Link>
+          </div>
         </div>
     )
 }
