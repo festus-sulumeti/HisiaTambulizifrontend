9d927763f8d7d0697b3b3a0c766dbd5ce2ac2259
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-04 16:25:52 +0300

    the updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import  { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>\
+    <BrowserRouter>
       <App />
     </BrowserRouter>
    
