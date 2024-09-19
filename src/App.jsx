d696e619623c6086490e848223cecf37f5c6bdc8
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-19 16:13:08 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-
-import "./App.css"
+import { AuthProvider } from './components/Auth/AuthContext'; // Import the AuthProvider
+import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
-import  Userhome from "./components/Userhome"
-import Forgotpassword from "./components/pages/Forgotpassword"
+import Userhome from "./components/Userhome";
+import Forgotpassword from "./components/pages/Forgotpassword";
+import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
 
 function App() {
   return (
-    <main className="pb-10">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/userhome" element={<Userhome />} />
-        <Route path="/forgot-password" element={<Forgotpassword/>} />
-      </Routes>
-    </main>
+    <AuthProvider>
+      <main className="pb-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/userhome" element={<ProtectedRoute element={<Userhome />} />} />
+          <Route path="/forgot-password" element={<Forgotpassword />} />
+        </Routes>
+      </main>
+    </AuthProvider>
   );
 }
 
