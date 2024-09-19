7899efed80663497892cd738a9acf325cfc0434c
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-19 06:53:39 +0300

    project files

--- src/components/Userhome.jsx
+++ src/components/Userhome.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
-import '../styling/Userhome.css'
+import '../styling/Userhome.css';
+import '../styling/mediaqueries/Userhome.css';
 
 const Userhome = () => {
-  // State to control when to display the video feed
+  
   const [startVideo, setStartVideo] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // Function to toggle the video stream
   const handleStartVideo = () => {
-    setStartVideo(true);
+    setIsLoading(true);
+    setTimeout(() => {
+      setStartVideo(true);
+      setIsLoading(false);
+    }, 1000); 
+  };
+
+  const handleStopVideo = () => {
+    setStartVideo(false);
   };
 
   return (
@@ -17,10 +26,15 @@ const Userhome = () => {
         <button className="start-button" onClick={handleStartVideo}>
           Start Video
         </button>
+      ) : isLoading ? (
+        <div className="loader"></div> 
       ) : (
         <div className="video-container">
           {/* Display the video feed when startVideo is true */}
           <img src="http://localhost:5000/video_feed" alt="Emotion Detection Feed" />
+          <button className="stop-button" onClick={handleStopVideo}>
+            Stop Video
+          </button>
         </div>
       )}
     </div>
