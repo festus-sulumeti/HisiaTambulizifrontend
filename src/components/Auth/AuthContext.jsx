812a28d96d342adf466f6facfc2f36a860d3edc7
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-19 16:15:12 +0300

    Add AuthContext for user authentication

---
+++ src/components/Auth/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  const login = () => {
+    setIsAuthenticated(true);
+    navigate('/userhome'); // Redirect to the home page after login
+  };
+
+  const signup = () => {
+    setIsAuthenticated(true);
+    navigate('/userhome'); // Redirect to the home page after signup
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    navigate('/'); // Redirect to the home page after logout
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, signup, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
