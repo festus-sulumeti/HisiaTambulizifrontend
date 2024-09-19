78fd3e9952fb7753be3e46cbd1a90353ac516cb4
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-19 16:20:43 +0300

    Add AuthContext for user authentication

--- src/components/Auth/AuthContext.jsx
+++ src/components/Auth/AuthContext.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
+// projeect files
+
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
